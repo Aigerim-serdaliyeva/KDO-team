ee3e3d4f10c9307b608dcc1223838ac3d5dbcc22
Aigerim Serdaliyeva <aigerim.serdaliyeva@mail.ru>
2019-09-09 14:50:36 +0600

    terms

--- app/js/main.js
+++ app/js/main.js
@@ -65,19 +65,6 @@ $(document).ready(function () {
     .addTo(controller);
 
 
-  // *** Animation of logo
-
-
-  // // create a scene
-  // new ScrollMagic.Scene({
-  //         duration: 100,    // the scene should last for a scroll distance of 100px
-  //         offset: 50    // start this scene after scrolling for 50px
-  //     })
-  //     .setPin("#my-sticky-element") // pins the element for the the scene's duration
-  //     .addTo(controller); // assign the scene to the controller
-
-
-
   // забираем utm из адресной строки и пишем в sessionStorage, чтобы отправить их на сервер при form submit
   var utms = parseGET();
   // проверяем есть ли utm в адресной строке, если есть то пишем в sessionStorage
